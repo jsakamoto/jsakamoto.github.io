cf4824a59b2f8c929a63180629286bf29373cd28
jsakamoto <junichi.jp@gmail.com>
2020-08-31 22:01:27 +0900

    Implement command line input behavior.

---
+++ App.razor.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+
+namespace CUIFlavoredPortfolioSite
+{
+    public partial class App
+    {
+        private ElementReference CommandLineInput;
+
+        private string CommandLineInputText { get; set; } = "";
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            await CommandLineInput.FocusAsync();
+        }
+
+        private async Task OnBlurCommandLineInput()
+        {
+            await CommandLineInput.FocusAsync();
+        }
+
+        private void OnKeyUpCommandLineInput(KeyboardEventArgs e)
+        {
+            if (e.Key == "Enter")
+            {
+                CommandLineInputText = "";
+            }
+        }
+    }
+}
