b780a8a518d2f62c67f89649cab1e8221f2849b2
jsakamoto <jsakamoto@users.noreply.github.com>
2021-06-18 07:04:40 +0000

    deploy: 2a9635bdade9c0b603d7afdc9e16d7afa02531fc

---
+++ scripts/helper.js
@@ -0,0 +1,29 @@
+"use strict";
+var Helper;
+(function (Helper) {
+    function scrollIntoView(element) {
+        element.scrollIntoView();
+    }
+    Helper.scrollIntoView = scrollIntoView;
+    var timerId = null;
+    window.addEventListener("mouseup", function (event) {
+        if (timerId !== null)
+            this.clearTimeout(timerId);
+        timerId = this.setTimeout(function () { resetFocus(); }, 400);
+    });
+    function resetFocus() {
+        var selection = window.getSelection();
+        if (selection !== null && selection.type === 'Range')
+            return;
+        var commandLineInput = document.getElementById('command-line-input');
+        if (commandLineInput !== null)
+            commandLineInput.focus();
+    }
+    document.addEventListener('keydown', function (e) {
+        if (e.key === 'ArrowUp' || e.key === 'ArrowDown' || e.key === 'Tab') {
+            if (e.srcElement.id === 'command-line-input') {
+                e.preventDefault();
+            }
+        }
+    });
+})(Helper || (Helper = {}));
