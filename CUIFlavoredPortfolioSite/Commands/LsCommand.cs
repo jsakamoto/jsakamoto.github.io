8b874135d0834c7db06ea7c3ae5ccfce7f61113f
jsakamoto <junichi.jp@gmail.com>
2021-12-12 14:17:56 +0900

    Improve "ls" command

--- CUIFlavoredPortfolioSite/Commands/LsCommand.cs
+++ CUIFlavoredPortfolioSite/Commands/LsCommand.cs
@@ -12,16 +12,35 @@ public class LsCommand : ICommand
 
     public void Invoke(IConsoleHost consoleHost, string[] args)
     {
-        var targetDir = Environment.CurrentDirectory;
-        var wildCard = "*.*";
+        var pathCollection = args.Skip(1).Where(p => !string.IsNullOrEmpty(p));
+        if (!pathCollection.Any()) pathCollection = pathCollection.Append(Path.Combine(Environment.CurrentDirectory, "*.*"));
 
-        var dirs = Directory.GetDirectories(targetDir, wildCard)
-            .Select(path => (IsDir: true, Name: Path.GetRelativePath(targetDir, path)));
-        var files = Directory.GetFiles(targetDir, wildCard)
-            .Select(path => (IsDir: false, Name: Path.GetRelativePath(targetDir, path)));
-        var entries = dirs.Concat(files)
-            .OrderBy(e => e.Name, StringComparer.Ordinal)
-            .Select(e => e.IsDir ? Blue(e.Name) : e.Name);
-        consoleHost.WriteLine(string.Join("  ", entries));
+        var multiEntries = pathCollection.Count() > 1;
+        var firstEntry = true;
+
+        foreach (var path in pathCollection)
+        {
+            var fullPath = Path.GetFullPath(path);
+            if (Directory.Exists(fullPath)) { fullPath = Path.Combine(fullPath, "*.*"); }
+            var targetDir = Path.GetDirectoryName(fullPath);
+            var wildCard = Path.GetFileName(fullPath);
+
+            var dirs = Directory.GetDirectories(targetDir, wildCard)
+                .Select(path => (IsDir: true, Name: Path.GetRelativePath(targetDir, path)));
+            var files = Directory.GetFiles(targetDir, wildCard)
+                .Select(path => (IsDir: false, Name: Path.GetRelativePath(targetDir, path)));
+            var entries = dirs.Concat(files)
+                .OrderBy(e => e.Name, StringComparer.Ordinal)
+                .Select(e => e.IsDir ? Blue(e.Name) : e.Name)
+                .ToArray();
+
+            if (!firstEntry) consoleHost.WriteLine();
+            if (multiEntries) consoleHost.WriteLine($"{path}:");
+
+            if (!entries.Any() && !wildCard.Contains('*') && !wildCard.Contains('?')) { consoleHost.WriteLine($"ls: cannot access '{path}': No such file or directory"); }
+            else { consoleHost.WriteLine(string.Join("  ", entries)); }
+
+            firstEntry = false;
+        }
     }
 }
