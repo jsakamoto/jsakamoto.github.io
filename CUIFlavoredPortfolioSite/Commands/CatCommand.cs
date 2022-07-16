da76565a0576416f19f3edb089ac47c4e1e9dad2
jsakamoto <junichi.jp@gmail.com>
2022-07-16 10:44:21 +0900

    Improve: make the "cat" command to be able to show "/dev/random"

--- CUIFlavoredPortfolioSite/Commands/CatCommand.cs
+++ CUIFlavoredPortfolioSite/Commands/CatCommand.cs
@@ -10,11 +10,8 @@ public class CatCommand : ICommand
 
     public string Description => "concatenate files and print on the standard output.";
 
-    public ValueTask InvokeAsync(IConsoleHost consoleHost, string[] args, CancellationToken cancellationToken)
+    public async ValueTask InvokeAsync(IConsoleHost consoleHost, string[] args, CancellationToken cancellationToken)
     {
-        const int bufferSize = 1024;
-        Span<byte> buffer = stackalloc byte[bufferSize];
-
         foreach (var path in args.Skip(1))
         {
             if (!File.Exists(path)) { consoleHost.WriteLine($"cat: {path}: No such file or directory"); continue; }
@@ -22,13 +19,26 @@ public class CatCommand : ICommand
             using var stream = File.OpenRead(path);
             for (; ; )
             {
-                var cbRead = stream.Read(buffer);
-                if (cbRead == 0) break;
-                var decodedText = Encoding.UTF8.GetString(buffer.Slice(0, cbRead));
-                consoleHost.Write(decodedText);
-                if (cbRead < bufferSize) break;
+                var shouldBeContinue = ProcessOneLine(stream, consoleHost, cancellationToken);
+                if (!shouldBeContinue) break;
+                await Task.Delay(1);
             }
         }
-        return ValueTask.CompletedTask;
+    }
+
+    private static bool ProcessOneLine(Stream stream, IConsoleHost consoleHost, CancellationToken cancellationToken)
+    {
+        const int bufferSize = 1024;
+        Span<byte> buffer = stackalloc byte[bufferSize];
+        var cbRead = stream.Read(buffer);
+        if (cbRead == 0) return false;
+
+        var decodedText = Encoding.UTF8.GetString(buffer.Slice(0, cbRead));
+        consoleHost.Write(decodedText);
+
+        if (cancellationToken.IsCancellationRequested) return false;
+        if (cbRead < bufferSize) return false;
+
+        return true;
     }
 }
