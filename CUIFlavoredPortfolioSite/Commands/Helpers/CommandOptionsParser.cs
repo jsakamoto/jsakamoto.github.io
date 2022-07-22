8fee4c1f2e30ac55a19c251434edbb6d0c0d58e9
jsakamoto <junichi.jp@gmail.com>
2022-07-22 22:00:54 +0900

    implement "rm" command

---
+++ CUIFlavoredPortfolioSite/Commands/Helpers/CommandOptionsParser.cs
@@ -0,0 +1,35 @@
+﻿namespace CUIFlavoredPortfolioSite.Commands.Helpers;
+
+public static class CommandOptionsParser
+{
+    public static bool TryParse<TOptions>(ref string[] args, out TOptions options, out string errorMessage) where TOptions : new()
+    {
+        options = new();
+        errorMessage = "";
+        var args2 = new List<string>();
+
+        var props = typeof(TOptions).GetProperties();
+        foreach (var arg in args)
+        {
+            if (!arg.StartsWith("-"))
+            {
+                args2.Add(arg);
+                continue;
+            }
+
+            foreach (var optionName in arg.ToCharArray().Skip(1))
+            {
+                var prop = props.FirstOrDefault(prop => prop.Name.ToLower().StartsWith(optionName));
+                if (prop == null)
+                {
+                    errorMessage = $"invalid option -- '{optionName}'";
+                    return false;
+                }
+                prop.SetValue(options, true);
+            }
+        }
+
+        args = args2.ToArray();
+        return true;
+    }
+}
---
+++ CUIFlavoredPortfolioSite/Commands/RmCommand.cs
@@ -0,0 +1,88 @@
+﻿using CUIFlavoredPortfolioSite.Commands.Helpers;
+using CUIFlavoredPortfolioSite.Services.CommandSet;
+using CUIFlavoredPortfolioSite.Services.ConsoleHost;
+
+namespace CUIFlavoredPortfolioSite.Commands;
+
+public class RmCommand : ICommand
+{
+    public IEnumerable<string> Names { get; } = new[] { "rm" };
+
+    public string Description => "remove files or directories";
+
+    public class RmCommandOptions
+    {
+        public bool Force { get; set; }
+        public bool Recursive { get; set; }
+    }
+
+    public async ValueTask InvokeAsync(IConsoleHost consoleHost, string[] args, CancellationToken cancellationToken)
+    {
+        args = args.Select(arg => arg.Trim()).Where(arg => arg != "").ToArray();
+
+        if (!CommandOptionsParser.TryParse<RmCommandOptions>(ref args, out var options, out var errors))
+        {
+            consoleHost.WriteLine($"rm: {errors}");
+            return;
+        }
+
+        if (args.Length < 2) { consoleHost.WriteLine("rm: missing operand"); return; }
+
+        foreach (var path in args.Skip(1).SelectMany(arg => GetFileSystemEntries(arg, consoleHost, options)))
+        {
+            var pathIsDirectory = Directory.Exists(path);
+            if (pathIsDirectory && !options.Recursive)
+            {
+                consoleHost.WriteLine($"rm: cannot remove '{path}': Is a directory");
+                continue;
+            }
+
+            await RemoveAsync(path, pathIsDirectory, options, cancellationToken);
+            if (cancellationToken.IsCancellationRequested) return;
+        }
+    }
+
+    private static IEnumerable<string> GetFileSystemEntries(string path, IConsoleHost consoleHost, RmCommandOptions options)
+    {
+        var dir = Path.GetDirectoryName(path);
+        dir = Path.GetFullPath(dir switch { "" => ".", null => "/", _ => dir });
+        var fileName = Path.GetFileName(path);
+        var fileSystemEntries = Directory.GetFileSystemEntries(dir, fileName);
+
+        if (!fileSystemEntries.Any())
+        {
+            if (!options.Force) consoleHost.WriteLine($"rm: cannot remove '{path}': No such file or directory");
+        }
+
+        return fileSystemEntries;
+    }
+
+    private static async ValueTask RemoveAsync(string path, bool pathIsDirectory, RmCommandOptions options, CancellationToken cancellationToken)
+    {
+        await Task.Delay(1);
+
+        if (!pathIsDirectory)
+        {
+            if (cancellationToken.IsCancellationRequested) return;
+            try { File.Delete(path); }
+            catch { if (!options.Force) throw; }
+            return;
+        }
+        else
+        {
+            var subDirectories = Directory.GetDirectories(path);
+            foreach (var subDirectory in subDirectories)
+            {
+                await RemoveAsync(subDirectory, pathIsDirectory: true, options, cancellationToken);
+            }
+            var files = Directory.GetFiles(path);
+            foreach (var file in files)
+            {
+                await RemoveAsync(file, pathIsDirectory: false, options, cancellationToken);
+            }
+            if (cancellationToken.IsCancellationRequested) return;
+            try { Directory.Delete(path); }
+            catch { if (!options.Force) throw; }
+        }
+    }
+}
